dc228161d09b7392727c10f69a20a70c5acabd27
Shreejai Raj <shreejai.raj@decidr.ai>
2026-03-10 08:50:46 +1100

    feat: add GSAP for animations and create first animation

---
+++ pages/01a_your-first-animation/script.js
@@ -0,0 +1,24 @@
+import gsap from 'gsap';
+
+gsap.to(".card", {
+  opacity : 1,
+  scale: 1,
+  duration: 1,
+  ease: "power1.Out",
+  onComplete: () => {
+    gsap.to(".card", {
+      y: -20,
+      repeat: -1,
+      yoyo: true,
+      duration: 0.5,
+    });
+
+    // Pulsing shadow
+    gsap.to(".card", {
+      boxShadow: "0 20px 80px rgba(29, 209, 161, 0.6)",
+      repeat: -1,
+      yoyo: true,
+      duration: 0.5,
+    })
+  }
+})
