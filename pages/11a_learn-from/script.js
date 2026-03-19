33df108c7ffa662d9641edd16d20bcd8e7a5b2b7
Shreejai Raj <shreejai.raj@decidr.ai>
2026-03-20 08:49:00 +1100

    feat: add GSAP animation for card elements with repeat functionality

---
+++ pages/11a_learn-from/script.js
@@ -0,0 +1,14 @@
+import gsap from "gsap";
+
+const repeat = document.querySelector(".repeat");
+
+const animation = gsap.from(".card", {
+  y: 60,
+  opacity: 0,
+  scale: 0.95,
+  delay: 0.2,
+  ease: "power4.out",
+  stagger: 0.2
+})
+
+repeat.addEventListener("click", () => {animation.restart()});
